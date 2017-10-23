d7dc891862dd6f3ed4d5893c73f6bbf791447838
Vishal-Sahu <vishsahu@gmail.com>
2017-10-24 01:20:41 +0530

    Added another JDBC code

--- grails-app/controllers/com/vertx/PublicController.groovy
+++ grails-app/controllers/com/vertx/PublicController.groovy
@@ -99,4 +99,49 @@ class PublicController {
         render "Success - 2"
     }
 
+
+    def jdbc1() {
+        def vertx = Vertx.vertx([
+                workerPoolSize: 40
+        ])
+        println "---1--0-----------"
+
+        JsonObject config = new JsonObject()
+                .put("url", "jdbc:mysql://localhost:3306/demo_lending?autoreconnect=true")
+                .put("user", "root")
+                .put("password", "nextdefault")
+                .put("driver_class", "com.mysql.jdbc.Driver")
+                .put("max_pool_size", 30);
+
+        def client = JDBCClient.createShared(vertx, config);
+        println "-----1-----------" + client
+
+        client.getConnection({ conn ->
+
+            def connection = conn.result()
+
+            // query some data with arguments
+            connection.query("select * from user where id < 20", { rs ->
+                if (rs.failed()) {
+                    println("Cannot retrieve the data from the database")
+                    rs.cause().printStackTrace()
+                    return
+                }
+
+                rs.result().results.each { line ->
+                    println("-----****---- : " + groovy.json.JsonOutput.toJson(line))
+                }
+
+                // and close the connection
+                connection.close({ done ->
+                    if (done.failed()) {
+                        throw new java.lang.RuntimeException(done.cause())
+                    }
+
+                })
+            })
+        })
+        println "----1-3-----------"
+        render "Success - 3"
+    }
 }
