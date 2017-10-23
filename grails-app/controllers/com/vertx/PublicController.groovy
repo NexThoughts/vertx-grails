b4183701a34ad99a9a4fa7876a7b3c72a2f83851
Vishal-Sahu <vishsahu@gmail.com>
2017-10-23 23:34:40 +0530

    Added code to run vertx server from grails action

---
+++ grails-app/controllers/com/vertx/PublicController.groovy
@@ -0,0 +1,20 @@
+package com.vertx
+
+import io.vertx.core.Vertx;
+
+class PublicController {
+
+    def index() {
+        def vertx = Vertx.vertx([
+                workerPoolSize: 40
+        ])
+
+        vertx.createHttpServer().requestHandler({ req ->
+            req.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello from Vert.x!")
+        }).listen(8084)
+
+        render "Success"
+    }
+}
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,31 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.net.NetServer;
+
 class BootStrap {
 
     def init = { servletContext ->
+
+//        Vertx vertx = Vertx.vertx();
+//
+////        def vertx = Vertx.vertx([
+////                workerPoolSize:40
+////        ])
+//
+//        def server = vertx.createNetServer()
+//        server.listen(0, "localhost", { res ->
+//            if (res.succeeded()) {
+//                println("***** Server is now listening on actual port: ${server.actualPort()}")
+//            } else {
+//                println("***** Failed to bind!")
+//            }
+//        })
     }
+
+
     def destroy = {
+
+
     }
 }
