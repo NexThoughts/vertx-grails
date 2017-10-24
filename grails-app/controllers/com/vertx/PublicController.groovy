e33903bcbd634d21dac2aea96b710f8053c39a80
Vishal-Sahu <vishsahu@gmail.com>
2017-10-24 13:22:22 +0530

    Added routing

--- grails-app/controllers/com/vertx/PublicController.groovy
+++ grails-app/controllers/com/vertx/PublicController.groovy
@@ -5,6 +5,8 @@ import io.vertx.ext.web.*;
 import io.vertx.ext.jdbc.*;
 import io.vertx.core.json.*;
 import io.vertx.ext.sql.*;
+import io.vertx.core.http.*;
+import io.vertx.ext.web.handler.*;
 
 class PublicController {
 
@@ -143,5 +145,121 @@ class PublicController {
         })
         println "----1-3-----------"
         render "Success - 3"
+    }
+
+    private def productList = new HashMap<>();
+
+    def route1() {
+
+        def vertx = Vertx.vertx([
+                workerPoolSize: 40
+        ])
+
+        setUpInitialData()
+
+        Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        router.get("/products/:productID").handler(this.&handleGetProduct);
+        router.put("/products/:productID").handler(this.&handleAddProduct);
+        router.get("/products").handler(this.&handleListProducts);
+
+        vertx.createHttpServer().requestHandler(router.&accept).listen(8086);
+
+        render "Success - 4"
+    }
+
+    private void handleGetProduct(RoutingContext routingContext) {
+        String productID = routingContext.request().getParam("productID");
+        HttpServerResponse response = routingContext.response();
+        if (productID == null) {
+            sendError(400, response);
+        } else {
+            JsonObject product = productList.get(productID);
+            if (product == null) {
+                sendError(404, response);
+            } else {
+                response.putHeader("content-type", "application/json").end(product.encodePrettily());
+            }
+        }
+    }
+
+    private void handleAddProduct(RoutingContext routingContext) {
+        String productID = routingContext.request().getParam("productID");
+        HttpServerResponse response = routingContext.response();
+        if (productID == null) {
+            sendError(400, response);
+        } else {
+            JsonObject product = routingContext.getBodyAsJson();
+            if (product == null) {
+                sendError(400, response);
+            } else {
+                productList.put(productID, product);
+                response.end();
+            }
+        }
+    }
+
+    private void handleListProducts(RoutingContext routingContext) {
+        JsonArray arr = new JsonArray();
+        productList.each{k, v ->
+            arr.add(v)
+        }
+        routingContext.response().putHeader("content-type", "application/json").end(arr.encodePrettily());
+    }
+
+    private void sendError(int statusCode, HttpServerResponse response) {
+        response.setStatusCode(statusCode).end();
+    }
+
+    private void setUpInitialData() {
+        addProduct(new JsonObject().put("id", "prod3568").put("name", "Egg Whisk").put("price", "3.99").put("weight", "150"));
+        addProduct(new JsonObject().put("id", "prod7340").put("name", "Tea Cosy").put("price", "5.99").put("weight", "100"));
+        addProduct(new JsonObject().put("id", "prod8643").put("name", "Spatula").put("price", "1.00").put("weight", "80"));
+    }
+
+    private void addProduct(JsonObject product) {
+        productList.put(product.getString("id"), product);
+    }
+
+
+    def route2() {
+        def route1 = router.route("/some/path/").handler({ routingContext ->
+
+            def response = routingContext.response()
+            // enable chunked responses because we will be adding data as
+            // we execute over other handlers. This is only required once and
+            // only if several handlers do output.
+            response.setChunked(true)
+
+            response.write("route1\n")
+
+            // Call the next matching route after a 5 second delay
+            routingContext.vertx().setTimer(5000, { tid ->
+                routingContext.next()
+            })
+        })
+
+        def route2 = router.route("/some/path/").handler({ routingContext ->
+
+            def response = routingContext.response()
+            response.write("route2\n")
+
+            // Call the next matching route after a 5 second delay
+            routingContext.vertx().setTimer(5000, { tid ->
+                routingContext.next()
+            })
+        })
+
+        def route3 = router.route("/some/path/").handler({ routingContext ->
+
+            def response = routingContext.response()
+            response.write("route3")
+
+            // Now end the response
+            routingContext.response().end()
+        })
+
+        render "Success - 5"
     }
 }
